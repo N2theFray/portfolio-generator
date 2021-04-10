880b00a7b12fa303050d9c81a60c7998a7f796b5
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-04-10 15:07:39 +0300

    dynamic creation

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
-
-const printProfileData = profileDataArr => {
-    for (let i=0; i<profileDataArr.length; i++){
-        console.log(profileDataArr[i])
-    }
-
-
-console.log('==============')
-
-profileDataArr.forEach(profileItem => console.log(profileItem))
-
-};
+const fs = require('fs');
+const [names, github] = profileDataArgs;
+const generatePage = require('./src/page-template.js')
 
 
 
-printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(names, github), err =>{
+    if (err) throw err;
+
+    console.log('Portfolio Complete! Check out index.html to see the output!')
+})
